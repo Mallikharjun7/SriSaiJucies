71086c6b86491ea9fff07a5be380abfd8a58e584
Mallikharjun <mallikharjun@example.com>
2025-06-05 16:17:51 +0530

    Update cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+        origin: ['https://srisaijucies-admin.onrender.com', 'https://srisaijucies-client.onrender.com'],
         methods: ['GET', 'POST'],
         credentials: true
     }
